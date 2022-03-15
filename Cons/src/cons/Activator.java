b063788bee364603d7f85684dbec9d2d392b77b2
AnudiDivarathna <anudithathsarani@gmail.com>
2022-03-15 20:24:47 +0530

    added checking methods

--- Cons/src/cons/Activator.java
+++ Cons/src/cons/Activator.java
@@ -23,10 +23,40 @@ public class Activator implements BundleActivator {
 		int select=pub.CheckEligibility(),selection;
 		if(select==1) {
 			System.out.println("Congragulations! Now you have to face the apitude test");
+		 
+			while(true) 
+			{
+				float mrk=pub.CheckApptitude(),mark;
+				if((mrk>100) || (mrk<0)) {
+					System.out.println("You entered invalid marks. Please re-enter");
+				}
+				else if(mrk>=75) {
+					System.out.println("Congradulations! You have passed the apptitude test. Now you can select a course");
+					break;
+				}
+				else {
+					System.out.println("You have failed the appitude test. Try in the next intake");
+					break;
+				}
+			}
+			
+			int stm=pub.CheckStream(),stream;
+			switch(stm){
+				case 1:
+				   System.out.println("You can choose these courses. \n Enginering\n Computing\n Buisness Managment");
+				   break;
+				case 2:
+				   System.out.println("You can choose these courses. \n Medicine\n Computing\n Buisness Management");
+				   break;
+				case 3:
+				   System.out.println("You can choose these courses. \n Computing\n Buisness Management");
+				   break;
+			}
+				
 			
 		}
 		else {
-			System.out.println("Sorry! please try in anothet time");
+			System.out.println("Sorry! please try in another time");
 		}
 	    
 	    
--- Pro/src/test/PubInt.java
+++ Pro/src/test/PubInt.java
@@ -5,5 +5,7 @@ package test;
 public interface PubInt {
 	public String publisherervice();
 	public int CheckEligibility();
+	public float CheckApptitude();
+	public int CheckStream();
 }
 
